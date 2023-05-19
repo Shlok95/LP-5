5207ca0c8420359f590113e13128c5d623edc8c5
Shlok95 <69349470+Shlok95@users.noreply.github.com>
2023-05-19 13:10:42 +0530

    Add files via upload

---
+++ Max and Avg.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+//#include <vector>
+#include <omp.h>
+#include <climits>
+using namespace std;
+
+void
+max_reduction (int arr[], int n)
+{
+  int max_value = INT_MIN;
+#pragma omp parallel for reduction(max: max_value)
+  for (int i = 0; i < n; i++)
+    {
+      if (arr[i] > max_value)
+	{
+	  max_value = arr[i];
+	}
+    }
+  cout << "Maximum value: " << max_value << endl;
+}
+
+void
+average_reduction (int arr[], int n)
+{
+  int sum = 0;
+#pragma omp parallel for reduction(+: sum)
+  for (int i = 0; i < n; i++)
+    {
+      sum += arr[i];
+    }
+  cout << "Average: " << (double) sum / (n - 1) << endl;
+}
+
+int
+main ()
+{
+  int *arr, n;
+  cout << "\n enter total no of elements=>";
+  cin >> n;
+  arr = new int[n];
+  cout << "\n enter elements=>";
+  for (int i = 0; i < n; i++)
+    {
+      cin >> arr[i];
+    }
+// int arr[] = {5, 2, 9, 1, 7, 6, 8, 3, 4};
+// int n = size(arr);
+  max_reduction (arr, n);
+  average_reduction (arr, n);
+}
